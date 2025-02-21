70549127c6e2ca1d7e4ae5a4f20c6e3aec713511
Oberdan Marinho <oberdanmarinho@gmail.com>
2025-02-20 21:11:39 -0300

    Pimeiro Commit do Projeto

---
+++ TechLibrary/TechLibrary.Api/Infraestructure/TechLibraryDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechLibrary.Api.Domain.Entities;
+
+namespace TechLibrary.Api.Infraestructure;
+
+public class TechLibraryDbContext : DbContext
+{
+	public DbSet<User> Users { get; set; }
+		
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		optionsBuilder.UseSqlite("Data Source=D:\\Studies\\Rocketseat\\Rocketseat.NlwConnect\\DataBase\\TechLibraryDb.db");
+	}
+}
